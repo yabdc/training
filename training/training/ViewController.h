56a662cad6dd62da31d3fdcedc2a4c3c1e332d66
yabdc <andychan@15002441Andy.local>
2015-06-10 09:38:20 +0800

    專案建立

---
+++ training/training/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  training
+//
+//  Created by 1500244 on 2015/6/10.
+//  Copyright (c) 2015年 andychan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ training/training/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  training
+//
+//  Created by 1500244 on 2015/6/10.
+//  Copyright (c) 2015年 andychan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
